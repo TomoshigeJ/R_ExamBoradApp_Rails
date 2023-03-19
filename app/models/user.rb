cd55a8329cc56db312b653c75dd38cbd88228e95
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-03-19 15:58:50 +0900

    add user_sessions
    dbマイグレート実行、user_sessionsコントローラ作成、ルーティング編集
    ビューファイル配置(未編集)、userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, length: {minmum: 3}
+  validates :first_name, presence: true, length: {maximum: 255}
+  validates :last_name, presence: true, length: {maximum: 255}
+  validates :email, presence: true, uniqueness: true
+
 end
--- db/migrate/20230319053743_sorcery_core.rb
+++ db/migrate/20230319053743_sorcery_core.rb
@@ -1,6 +1,8 @@
 class SorceryCore < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :first_name, limit: 255, null: false
+      t.string :last_name, limit: 255, null: false
       t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
