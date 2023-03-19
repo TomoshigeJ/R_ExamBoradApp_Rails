c8ee3a4f664d0ec7c8af6cf8125b32ec4b3d2233
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-03-19 17:45:43 +0900

    Create userコントローラ
    userコントローラにnew,create,user_paramsメソッド追加
    ルーティング追加:users
    userモデルのバリデーション修正
    ユーザー新規登録ビューの追加と修正(form_withとlink_to)
    ログインページビューの修正(aタグをlink_toへ)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :password, length: {minmum: 3}
+  validates :password, length: {minimum: 3}
   validates :first_name, presence: true, length: {maximum: 255}
   validates :last_name, presence: true, length: {maximum: 255}
   validates :email, presence: true, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: [:new, :create]
 end
