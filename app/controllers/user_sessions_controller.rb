55d56f05d5c3990c3d847402abf1f22d1a6933b6
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-03-24 20:27:32 +0900

    Finish Test後
    間違っている箇所の修正
    フラッシュメッセージはi18nで

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,16 +7,16 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, success: 'ログインしました'
+      redirect_back_or_to root_path, success: t('.success')
     else
-      flash.now[:danger] = 'ログインに失敗しました'
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
 
   def destroy
     logout
-    flash[:success] = 'ログアウトしました'
+    flash[:success] = success: t('.success')
     redirect_to root_path
   end
 
