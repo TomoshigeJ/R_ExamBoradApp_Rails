e21308c887044b776a01ee7a8562bb55744eb048
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-03-28 20:33:08 +0900

    Update 課題完了後の修正
    アソシエーション活用しきれていない(current_user.boards.build(board_params))
    form_withのテキストエリア
    フラッシュメッセージの翻訳ファイルの取得方法(翻訳ファイル)
    has_many :board →複数形になっていなかった
    掲示板作成ビューを単一ファイルで作成していたのでフォーム用ファイルを分ける

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,19 +8,23 @@ class BoardsController < ApplicationController
   end
 
   def create
-    @board = Board.new(board_params)
+    @board = current_user.boards.build(board_params)
     if @board.save
-      redirect_to boards_path, success: t('.success')
+      redirect_to boards_path, success: t('defaults.message.created', item: Board.model_name.human)
     else
-      flash.now[:danger] = t('.fail')
+      flash.now[:danger] = t('defaults.message.not_created', item: Board.model_name.human)
       render :new
     end
+  end
+
+  def show
+    @board = Board.find(params[:id])
   end
 
   private
 
   def board_params
-    params.require(:board).permit(:title, :body,).merge(user_id: current_user.id)
+    params.require(:board).permit(:title, :body,)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: [:new, :create]
-  resources :boards, only: [:index, :new, :create]
+  resources :users, only: %i[new create]
+  resources :boards, only: %i[index new create]
 end
