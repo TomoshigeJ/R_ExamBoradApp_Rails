fc7483f85913e0a71f32d5f2ae0e2a5ec7594ff7
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-03 12:37:56 +0900

    Update helperを使ってみる
    各ビューファイルとレイアウトファイルからメソッド呼び出す

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_title(title)
+    content_for(:title) { title }
+  end
+
+  def yield_for(content_symbol, default = '')
+    content_for?(content_symbol) ? "#{content_for(content_symbol)} | #{default}" : default
+  end
 end
