5c3f1b241c70bcd0072418cc505452ff331d6002
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-19 10:46:41 +0900

    Update プロフィールshowへのリンク
    profilesコントローラとビューファイルの修正から

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,18 @@
 class ProfilesController < ApplicationController
+  # before action :set_user, only: %i[edit show]
+
   def edit
   end
 
   def show
+    
+    binding.pry
+    
+  end
+
+  private
+
+  def set_user
+    # @user = User.find(current_user.id)
   end
 end
