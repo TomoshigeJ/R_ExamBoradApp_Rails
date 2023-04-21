219957cc7ab0f4c1722965d5eb5be82bbf8a5858
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-21 09:38:50 +0900

    Update profiles更新関連
    editビューファイル修正
    updateアクション処理作成
    動作チェック

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,15 @@
 class ProfilesController < ApplicationController
-  before_action :set_user, only: %i[edit show]
+  before_action :set_user, only: %i[edit update show]
 
   def edit; end
 
   def update
-    
+    if @user.update(profiles_params)
+      redirect_to profiles_path, success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: User.model_name.human)
+      render :edit
+    end
   end
 
   def show; end
@@ -14,4 +19,9 @@ class ProfilesController < ApplicationController
   def set_user
     @user = User.find(current_user.id)
   end
+
+  def profiles_params
+    params.require(:user).permit(:email, :last_name, :first_name, :avatar, :avatar_cache)
+  end
+
 end
