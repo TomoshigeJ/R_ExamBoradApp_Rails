a10dc11a8638e16d176847226535c7a6f0efaf8d
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-23 14:21:55 +0900

    Update パスワードリセット要求後のリダイレクト先、成功メッセージ変更

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,7 +6,7 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     @user.deliver_reset_password_instructions! if @user
-    redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
+    redirect_to login_path, success: t('.success')
   end
 
   def edit
