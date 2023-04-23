38a8e1fa768580ac104d1dcca25bffa6a3c7e7da
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-23 16:50:48 +0900

    Update 課題完了後の修正
    間違っている箇所を修正
    メール再送信できる時間10秒後に変更
    user.rbのバリデーション追加(パスワード再登録時に空白でエラーを出す)

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,37 +4,31 @@ class PasswordResetsController < ApplicationController
   def new; end
 
   def create
-    @user = User.find_by_email(params[:email])
-    @user.deliver_reset_password_instructions! if @user
+    @user = User.find_by(email: params[:email])
+    # ぼっち演算子(&.)にて存在しないユーザー(nil)であっても例外を発生させない
+    @user&.deliver_reset_password_instructions!
+    # 「存在しないメールアドレスです」という旨の文言を表示すると、逆に存在するメールアドレスを特定されてしまうため、
+    # あえて成功時のメッセージを送信させている
     redirect_to login_path, success: t('.success')
   end
 
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
-
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    not_authenticated if @user.blank?
   end
 
   def update
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
 
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    return not_authenticated if @user.blank?
 
     @user.password_confirmation = params[:user][:password_confirmation]
-
     if @user.change_password(params[:user][:password])
       redirect_to login_path, success: t('.success')
     else
       render :action => "edit"
     end
   end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
+  # パスワード再設定時に空白でも登録ができてしまう現象に対応
+  validates :password, presence: true, length: {minimum:3}, if: -> { new_record? || changes[:crypted_password] || changes[:reset_password_token] }
+
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true, length: {maximum: 255}
   validates :last_name, presence: true, length: {maximum: 255}
