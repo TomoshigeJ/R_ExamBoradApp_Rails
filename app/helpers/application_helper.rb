371d238d7b330bc4f7ca7f8431d1b743bb4ed7c2
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-30 19:48:59 +0900

    Update メニューのアクティブ機能

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def active?(controller_name)
+    return 'active' if controller_name.include?(params[:controller])
+  end
 end
