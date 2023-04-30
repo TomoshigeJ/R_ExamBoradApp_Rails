427098b10c96c9769f399fbf1560a1111ca7a354
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-30 15:04:02 +0900

    Update ユーザー編集機能まで

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -11,13 +11,4 @@ class Admin::BaseController < ApplicationController
   def check_admin
     redirect_to root_path, warning: t('defaults.message.not_authorized') unless current_user.admin?
   end
-
-  def set_q
-    @q = User.ransack(params[:q])
-  end
-
-  def set_users
-    @users = @q.result(distinct: true).page(params[:page])
-  end
-
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 class Admin::UsersController < Admin::BaseController
-  before_action :set_q, only: [:index]
-  before_action :set_users, only: [:index]
+  before_action :set_q, only: %i[index]
+  before_action :set_users, only: %i[index]
+  before_action :set_user, only: %i[show edit update destroy]
 
   def index; end
 
@@ -12,9 +13,32 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: User.model_name.human)
+      render :edit
+    end
   end
 
   def destroy
   end
 
+  private
+
+  def set_q
+    @q = User.ransack(params[:q])
+  end
+
+  def set_users
+    @users = @q.result(distinct: true).page(params[:page])
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :last_name, :first_name, :avatar, :avatar_cache, :role)
+  end
 end
